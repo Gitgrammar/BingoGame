d1b9a85030d8d12ff3d263d4f381b4a2c7fb7e8c
hogehoge <elementrd6@hotmail.com>
2022-11-24 13:46:10 +0900

    残念でしたまで追加

--- BingoApp.java
+++ BingoApp.java
@@ -29,9 +29,15 @@ public class BingoApp {
          }
          System.out.println();
          
+         }
+         if(array[0][0]==array[0][1]||array[0][0]==array[0][1]){
+            System.out.println("やったね");
+         }else{
+            System.out.println("残念でした。");
+         }
          
    }
     
    } 
 }
-}
+
