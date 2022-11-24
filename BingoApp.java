b489264f0f21c6ee5e16dca807eae48a61f8ff09
hogehoge <elementrd6@hotmail.com>
2022-11-25 08:58:47 +0900

    ビンゴになる。

--- BingoApp.java
+++ BingoApp.java
@@ -15,7 +15,7 @@ public class BingoApp {
   
 
  
-   
+   while(true){
     if(BET==0){
       System.out.println("終了します。");
     }
@@ -30,21 +30,23 @@ public class BingoApp {
          System.out.println();
          
          }
-         if(array[0][0]==array[0][1]||array[0][0]==array[0][2]
-           &&array[1][0]==array[1][1]||array[1][0]==array[1][2]
-            &&array[2][0]==array[2][1]||array[2][0]==array[2][2] 
-            &&array[0][0]==array[1][0]||array[0][0]==array[2][0]
-            &&array[0][1]==array[1][1]||array[0][1]==array[2][1]
-            &&array[0][2]==array[1][2]||array[1][0]==array[2][2]
-            &&array[0][0]==array[1][1]||array[0][0]==array[2][2]
-            &&array[0][2]==array[1][1]||array[0][2]==array[2][0]
+         if(array[0][0]==array[0][1]&&array[0][0]==array[0][2]
+           ||array[1][0]==array[1][1]&&array[1][0]==array[1][2]
+            ||array[2][0]==array[2][1]&&array[2][0]==array[2][2] 
+            ||array[0][0]==array[1][0]&&array[0][0]==array[2][0]
+            ||array[0][1]==array[1][1]&&array[0][1]==array[2][1]
+            ||array[0][2]==array[1][2]&&array[0][2]==array[2][2]
+            ||array[0][0]==array[1][1]&&array[0][0]==array[2][2]
+            ||array[0][2]==array[1][1]&&array[0][2]==array[2][0]
          )
          {
             System.out.println("やったね");
+            break;
          }else{
             System.out.println("残念でした。");
          }
       }
+   }
    
     
    } 
