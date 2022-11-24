3c3f77e378a0a7a7dabeced2df54f67f9798b447
hogehoge <elementrd6@hotmail.com>
2022-11-24 12:13:18 +0900

    ２重ループへ前進

--- BingoApp.java
+++ BingoApp.java
@@ -11,21 +11,19 @@ public class BingoApp {
     System.out.print("BET枚数を入力。0で終了　1-100>");
     int BET=sc.nextInt();
     Random rand=new Random();
-    int i=0;
-    int j=0;
-   int [][]array=new int[i][j];
-
+  
+    int [][]array=new int[3][3];
    
     if(BET==0){
       System.out.println("終了します。");
     }
    else{
-      for(i=0;i<=0;i++){
-         for(j=0;j<=i;j++){
+      for(int i=0;i<=array.length;i++){
+         for(int j=0;j<=i;j++){
              int pickedNum=rand.nextInt(9);
              array[i][j]=pickedNum;
-             System.out.println(array[i][j]);
          }
+         System.out.println(Arrays.toString(array));
          System.out.println();
    }
     
