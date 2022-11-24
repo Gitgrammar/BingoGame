58843449b9df0ce2ab9e060870332ab9efa8a49f
hogehoge <elementrd6@hotmail.com>
2022-11-24 13:09:16 +0900

    数字まで出てくる

--- BingoApp.java
+++ BingoApp.java
@@ -11,19 +11,25 @@ public class BingoApp {
     System.out.print("BET枚数を入力。0で終了　1-100>");
     int BET=sc.nextInt();
     Random rand=new Random();
-  
     int [][]array=new int[3][3];
+  
+
+ 
    
     if(BET==0){
       System.out.println("終了します。");
     }
    else{
-      for(int i=0;i<=array.length;i++){
+      System.out.println("0以外が押された");
+      for(int i=0;i<=3;i++){
          for(int j=0;j<=i;j++){
-             int pickedNum=rand.nextInt(9);
+            int pickedNum=rand.nextInt(32);
+            System.out.println(pickedNum);
              array[i][j]=pickedNum;
+             System.out.println(array[i][j]);
          }
-         System.out.println(Arrays.toString(array));
+         
+         
          System.out.println();
    }
     
