2f9efcc482f740e8bfac9674cd92ac532a2110da
hogehoge <elementrd6@hotmail.com>
2022-11-24 14:01:19 +0900

    前と後ろが合ってればやったね

--- BingoApp.java
+++ BingoApp.java
@@ -30,14 +30,24 @@ public class BingoApp {
          System.out.println();
          
          }
-         if(array[0][0]==array[0][1]||array[0][0]==array[0][1]){
+         if(array[0][0]==array[0][1]||array[0][0]==array[0][2]
+           &&array[1][0]==array[1][1]||array[1][0]==array[1][2]
+            &&array[2][0]==array[2][1]||array[2][0]==array[2][2] 
+            &&array[0][0]==array[1][0]||array[0][0]==array[2][0]
+            &&array[0][1]==array[1][1]||array[0][1]==array[2][1]
+            &&array[0][2]==array[1][2]||array[1][0]==array[2][2]
+            &&array[0][0]==array[1][1]||array[0][0]==array[2][2]
+            &&array[0][2]==array[1][1]||array[0][2]==array[2][0]
+         )
+         {
             System.out.println("やったね");
          }else{
             System.out.println("残念でした。");
          }
-         
-   }
+      }
+   
     
    } 
-}
 
+
+}
