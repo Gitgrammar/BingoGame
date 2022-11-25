f8920db4fb5047c76fc7a45269041b6a41195ac5
hogehoge <elementrd6@hotmail.com>
2022-11-25 09:26:11 +0900

    新たにwhile追加

--- BingoApp.java
+++ BingoApp.java
@@ -8,6 +8,8 @@ public class BingoApp {
     System.out.print("年齢を入れてください。>>");
     int age=sc.nextInt();
     int coin=100;
+    while(true){
+   System.out.printf("%sさんの所持コイン:%d\n",name,coin);
     System.out.print("BET枚数を入力。0で終了　1-100>");
     int BET=sc.nextInt();
     Random rand=new Random();
@@ -15,12 +17,11 @@ public class BingoApp {
   
 
  
-   while(true){
+
     if(BET==0){
       System.out.println("終了します。");
     }
    else{
-      System.out.println("0以外が押された");
       for(int i=0;i<=2;i++){
          for(int j=0;j<=2;j++){
             int pickedNum=rand.nextInt(9);
